acf9c94b9e630e491826436d97327dea9f8ea4c4
Anokhi Kastia <anokhikastia@berkeley.edu>
2017-06-12 22:57:21 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,4 +1,4 @@
-class HangpersonGame
+class HangpersonGame 
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
@@ -8,8 +8,13 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @wrong_chances = 0
   end
 
   def self.get_random_word
@@ -18,5 +23,45 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
-end
+  
+  def guess(letter)
+    raise ArgumentError if letter.nil?
+    raise ArgumentError if letter.empty?
+    raise ArgumentError if letter =~ /[^a-zA-Z]+/
+    letter.downcase!
+    g = @guesses.include?(letter)
+    ng = @wrong_guesses.include?(letter)
+    if g or ng
+      return false 
+    elsif @word.include? (letter)
+      @guesses << letter
+      return true 
+    else
+      @wrong_guesses << letter
+      @wrong_chances += 1
+      return true
+    end 
+  end 
+  
+  def word_with_guesses
+    output = ''
+    @word.split("").each do |letter|
+      if @guesses.include? letter 
+        output << letter
+      else
+        output << '-'
+      end 
+    end 
+    return output
+  end 
+  
+  def check_win_or_lose
+    if (@guesses.length == @word.length) and @wrong_chances < 7
+      return :win 
+    elsif @wrong_chances >= 7
+      return :lose
+    else 
+      return :play 
+    end 
+  end 
+end 
